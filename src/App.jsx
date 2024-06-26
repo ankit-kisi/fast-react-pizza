06fc26d26c43706dff1b80afe4d46e13949cffc0
Ankit Kisi <ankitkisi@Ankits-MacBook-Air.local>
2024-06-26 19:16:34 +0545

    created AppLayout with Routes

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+import AppLayout from "./ui/AppLayout";
+
+//this imperative way is required to enable data fetching and loading
+const router = createBrowserRouter([
+  {
+    // there's no path so technically called layout path
+    element: <AppLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/menu", element: <Menu /> },
+      { path: "/cart", element: <Cart /> },
+      { path: "/order/new", element: <CreateOrder /> },
+      { path: "/order/:orderId", element: <Order /> },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
